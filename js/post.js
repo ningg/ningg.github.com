76ed0b4907f358675e8485c5504b62f7aab7d74a
Ning Guo <guoning.gn@gmail.com>
2014-06-13 01:31:07 +0800

    display the comments with onload

--- js/post.js
+++ js/post.js
@@ -26,11 +26,17 @@ $(document).ready(function(){
     //***********************
     //**评论的代码也删掉哦***
     window.disqus_shortname = 'ningg'; // required: replace example with your forum shortname(register on DISQUS)
-    $('#disqus_container .comment').on('onload',function(){
+    
+	(function() {
+         var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+         dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+     })();
+	/*$('#disqus_container .comment').on('click',function(){
         $(this).html('加载中...');
         var that = this;
         $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    });
+    });*/
     //**评论的代码也删掉哦***
     //***********************
 
