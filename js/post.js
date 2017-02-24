8720885f177dafa96fa62f74ee4b19470b9a305b
Guo Ning <guoning.gn@gmail.com>
2017-02-24 22:53:04 +0800

    fix: duoshuo comment config

--- js/post.js
+++ js/post.js
@@ -203,23 +203,5 @@ $(document).ready(function(){
         $.getScript('/js/css3-ani.js',function(){});
     }
 
-    //***********************
-    //**评论的代码也删掉哦***
-    <!-- 多说公共JS代码 start (一个网页只需插入一次) -->
-	<script type="text/javascript">
-	var duoshuoQuery = {short_name:"ningg"};
-		(function() {
-			var ds = document.createElement('script');
-			ds.type = 'text/javascript';ds.async = true;
-			ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.unstable.js';
-			ds.charset = 'UTF-8';
-			(document.getElementsByTagName('head')[0] 
-			 || document.getElementsByTagName('body')[0]).appendChild(ds);
-		})();
-		</script>
-    <!-- 多说公共JS代码 end -->
-    //**评论的代码也删掉哦***
-    //***********************
-
 
 });
