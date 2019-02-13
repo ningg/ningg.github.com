e98c0663a02e05facc5ebea901066f3829538b10
Ning Guo <guoning.gn@gmail.com>
2019-02-14 01:58:00 +0800

    fix: 从原始版本中,删除 _site 下的编辑文件

--- _site/README.md
+++
@@ -1,9 +0,0 @@
-# 动手搭建自己的博客
-
-我写了一篇博客[GitHub上搭建个人网站](http://ningg.github.io/build-blog-with-github/)， 需要的可以读一下。
-
-# ChangeLog
-
-* 2014年5月，基于[BeiYuu](http://beiyuu.com/) & [Havee](http://havee.me/) 的博客模版，改进出此博客
-* 2013年8月，基于[BeiYuu](http://beiyuu.com/) 博客模版，在GitHub上建立个人网站
-* 2012年9月，在内网使用Wordpress搭建个人博客
--- _site/images/imageDeal.bat
+++
@@ -1,9 +0,0 @@
-/********************************************************************************/
-/********** This bat file is used to process the picture of the blog ************/
-/************************ author: Ning Guo **************************************/
-/********************* Environment: win xp, ImageMagick *************************/
-/********************************************************************************/
-@echo off
-find . -name "*.jpg" | xargs -n1 mogrify -format jpg -strip +profile "*" -quality 85 -resize ">670"
-echo "Image convert SUCCESS! Press any key to continue..."
-echo. & pause
